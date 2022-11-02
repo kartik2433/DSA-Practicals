eaf6ed3f99cc5d41fd048b890d231072131353b7
Kartik Bhatiya <95977507+kartik2433@users.noreply.github.com>
2022-11-02 22:46:07 +0530

    Updated DSA-3.cpp

--- DSA-3.cpp
+++ DSA-3.cpp
@@ -2,39 +2,38 @@
 //      PUSH, POP, PEEK, CHANGE, DISPLAY
 
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 int MAXSIZE = 8;       
 int stack1[8],stack2[8];     
 int top1 = -1;
 int top2 = -1;
 
-// int isempty(int top) {
+int isEmpty(int top) {
 
-//    if(top == -1)
-//       return 1;
-//    else
-//       return 0;
-// }
+   if(top == -1)
+      return 1;
+   else
+      return 0;
+}
    
-// int isfull(int top) {
+int isFull(int top) {
 
-//    if(top == MAXSIZE)
-//       return 1;
-//    else
-//       return 0;
-// }
+   if(top == MAXSIZE)
+      return 1;
+   else
+      return 0;
+}
 
-int peek1() {
+int peek() {
    return stack1[top1];
 }
 
-int pop1() {
+int pop(int stack[],int &top) {
    int data;
-	
-   if(top1!=(-1)) {
-      data = stack1[top1];
-      top1 = top1 - 1;   
+   if(!isEmpty(top)) {
+      data = stack[top];
+      top--;   
       return data;
    } 
    else {
@@ -42,81 +41,84 @@ int pop1() {
    }
 }
 
-int pop2() {
-   int data;
-	
-   if(top2!=(-1)) {
-      data = stack2[top2];
-      top2 = top2 - 1;   
-      return data;
+int push(int stack[],int &top,int data) {
+   if(!isFull(top)) {
+       top++; 
+       stack[top] = data;
    } 
    else {
-      cout<<"Stack is empty.\n";
-   }
-}
-
-int push1(int data) {
-
-   if(top1!=MAXSIZE) {
-      top1 = top1 + 1;   
-      stack1[top1] = data;
-   } else {
       cout<<"Stack is full.\n";
    }
 }
 
-int push2(int data) {
-
-   if(top2!=MAXSIZE) {
-      top2 = top2 + 1;   
-      stack2[top2] = data;
-   } else {
-      cout<<"Stack is full.\n";
-   }
-}
-
-void print()
+void print(int top)
 {
-   while(top1>=0) {
-      int data = pop1();
+   while(!isEmpty(top)) {
+      int data = pop(stack1,top);
       cout<<data<<endl;
    }
 }
 
 int main() {
    // push items on to the stack 
-   int n,ele,index;
+   int n,ele,del,flag=0;
+   int choice;
    cout<<"Enter Number Of Elements: ";
    cin>>n;
    cout<<"Enter Elements: ";
    for(int i=0;i<n;i++)
    {
       cin>>ele;
-      push1(ele);
+      push(stack1,top1,ele);
    }
-   cout<<"Element at top of the stack:"<<peek1()<<endl;
-   cout<<"Enter Index Of Element You Want To Change:"<<endl;
-   cin>>index;
-   for(int i=top1; i>index; i--)
-   {
-      int data;
-      data=pop1();
-      push2(data);
-   }
-   pop1();
-   cout<<"Enter Element: "<<endl;
-   cin>>ele;
-   cout<<endl<<endl;
-   push1(ele);
-   for(int i=top2; i>=0; i--)
-   {
-      int data;
-      data=pop2();
-      push1(data);
-   }
+   while(top1>=-1) {
+   cout<<"1.DISPLAY"<<endl<<"2.PEEK"<<endl<<"3.CHANGE"<<endl<<"4.EXIT"<<endl;
+   cin>>choice;
    cout<<endl;
-   print();
-   // cout<<"Stack full: "<<(isfull()?"true":"false")<<endl;
-   // cout<<"Stack empty: "<<(isempty()?"true":"false"); 
-   return 0;
+   switch(choice) {
+      case 1:
+         print(top1);
+         break;
+      case 2:
+         cout<<"Peek Element is: "<<peek()<<endl;
+         break;
+      case 3:
+        { cout<<"Enter Element To Change: ";
+         cin>>del;
+         while(!isEmpty(top1))
+         {
+            int data;
+            data=pop(stack1,top1);
+            if(data==del)
+            {
+               flag=1;
+               break;
+            }
+            else{
+               push(stack2,top2,data);
+            }
+         }
+         if(flag==0)
+         {
+            cout<<"Not Found: ";
+         }
+         else
+         {
+            cout<<"Enter New Element: ";
+            cin>>ele;
+            push(stack1,top1,ele);
+         }
+         while(!isEmpty(top2))
+         {
+            int data;
+            data=pop(stack2,top2);
+            push(stack1,top1,data);
+         }}
+         break;
+      case 4:
+          return 0;
+      default:
+         cout<<"Enter Valid Choice: "<<endl;
+   }
+}
 }
